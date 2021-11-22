4a181e6220fdfed57079202a96729a3fd30e9b27
jonghyun <jonghyun9606@gmail.com>
2021-11-22 18:38:53 +0900

    Edit login, Add createAccount

---
+++ src/login_check.php
@@ -0,0 +1,10 @@
+<?php
+    $id_val = $_POST["id_val"];
+    $pw_val = $_POST["pw_val"];
+
+    print("ID : " . $id_val ."<br>");
+    print("Passward : " . $pw_val);
+
+
+
+?>
---
+++ src/login_script.js
@@ -0,0 +1,3 @@
+function printHello(){
+    document.write(111)
+}
