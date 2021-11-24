b88564274aed6f63ed5ca3e93e819ad38b3fc231
jonghyun <jonghyun9606@gmail.com>
2021-11-24 17:31:27 +0900

    Edit page structure

---
+++ src/shop_account_admin.php
@@ -0,0 +1,11 @@
+<?php
+    echo $_POST['usr_id']. "<br>";
+    echo $_POST['usr_pw']. "<br>";
+    echo $_POST['usr_name']. "<br>";
+    echo $_POST['usr_email']. "<br>";
+    echo $_POST['usr_phoneNum']. "<br>";
+    echo $_POST['usr_addPost']. "<br>";
+    echo $_POST['usr_addAddress']. "<br>";
+    echo $_POST['usr_addDetail']. "<br>";
+    echo $_POST['usr_addExtra'];
+?>
