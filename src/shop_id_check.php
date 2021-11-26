d8a9f3a5bad733de153ad4604e8444d860ee5135
jonghyun <jonghyun9606@gmail.com>
2021-11-26 19:35:40 +0900

    Edit mysql

--- src/shop_id_check.php
+++ src/shop_id_check.php
@@ -3,12 +3,23 @@
 
     if ($post_data->id == '') die('false');
 
-    
+    $host = '192.168.80.130';
+    $user = 'adminjonghyun';
+    $pw = 'tjwhdgus';
+    $dbname = 'robo_db';
+    $conn = mysqli_connect($host, $user, $pw, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
     $result = null;
+
+    $sql = "SELECT user_id FROM user_table where user_id ='".$post_data->id."'";
+    $result = mysqli_query($conn,$sql);
     // try {
     //     //여기서 데이터 베이스 훑어보기
-    //     //$result = DB::query('SELECT * FROM user WHERE user_id=:id', array(':id' => $post_data->id));
+    //     $result = DB::query('SELECT * FROM user_table WHERE user_id=:id', array(':id' => $post_data->id));
     // } catch (Exception $e) {
     //     // 쿼리에서 에러 발생 시 false로 처리
     //     die('false');
@@ -16,12 +27,12 @@
 
     //데이터 베이스에서 중복되는걸 찾으면 result값이 0보다커짐
     //count($result) > 0
-    if ($result == null) {
+    if (count($result) > 0) {
         //중복안됨
-        echo 'true';
+        echo 'false';
     } else {
         //중복됨
-        echo 'false';
+        echo 'true';
     }
 
 ?>
